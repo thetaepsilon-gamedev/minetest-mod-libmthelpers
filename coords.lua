3d9a49d58ac41eafbc0210ec20d623de00d44c8e
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-09-25 19:33:17 +0100

    coords.lua: fix missing local for valtype inside coords.format()'s nested function

--- coords.lua
+++ coords.lua
@@ -4,8 +4,9 @@ coords.format = function(vec, sep, start, tail)
 	sep = sep or ","
 	start = start or "("
 	tail = tail or ")"
+
 	local coord = function(val)
-		valtype = type(val)
+		local valtype = type(val)
 		local ret = ""
 		if valtype == nil then
 			ret = "???"
@@ -16,6 +17,7 @@ coords.format = function(vec, sep, start, tail)
 		end
 		return ret
 	end
+
 	return start..coord(vec.x)..sep..coord(vec.y)..sep..coord(vec.z)..tail
 end
 
