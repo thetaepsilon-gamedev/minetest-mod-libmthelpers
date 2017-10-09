01f9b03f67686e90eee89ae4094c5d9995535ad1
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-10-09 22:41:19 +0100

    datastructs.lua: add set iterator-returned element checking

--- datastructs.lua
+++ datastructs.lua
@@ -43,6 +43,24 @@ end
 
 
 local mkassert = modhelpers.check.mkassert
+local listequal = modhelpers.check.listequaltest
+
+local collect_iterator = function(it, results)
+	while true do
+		local result = it.next()
+		if result ~= nil then
+			table.insert(results, result)
+		else
+			break
+		end
+	end
+end
+
+local collect_set = function(set)
+	local results = {}
+	collect_iterator(set.iterator(), results)
+	return results
+end
 
 -- maybe I could make this runnable outside MT...
 datastructs.selftest.set = function()
@@ -51,6 +69,9 @@ datastructs.selftest.set = function()
 	local assert = mkassert(name)
 	local set = datastructs.new.set()
 	local t = {}
+	local collected = nil
+
+	assert(#collect_set(set) == 0, "new set should return zero elements")
 
 	local notexists = "object should not be considered present if not previously inserted"
 	assert(not set.ismember(t), notexists)
@@ -58,7 +79,26 @@ datastructs.selftest.set = function()
 
 	assert(set.add(t), "object should be newly inserted")
 	assert(set.ismember(t), "object should be member after being inserted")
+	collected = collect_set(set)
+	assert(#collected == 1, "set should return one element after insertion")
+	assert(collected[1] == t, "sole item of set should be the inserted element")
 	assert(set.remove(t), "object should have been removed after being inserted")
+	assert(#collect_set(set) == 0, "set should return zero elements after removal")
+
+	local oldelement = "item should not be present if added then removed"
+	assert(not set.remove(t), oldelement)
+	assert(not set.ismember(t), oldelement)
+
+	local newelement = "set should be able to accept a new element"
+	assert(set.add(1), newelement)
+	assert(set.add(2), newelement)
+	assert(set.add(3), newelement)
+	assert(#collect_set(set) == 3, "set should contain three elements after three insertions")
+	assert(set.remove(2), "object should have been removed")
+	assert(#collect_set(set) == 2, "set should contain two elements after three insertions and one removal")
+
+	assert(not set.remove(4), notexists)
+	assert(not set.ismember(4), notexists)
 
 	return name.." self-tests completed"
 end
