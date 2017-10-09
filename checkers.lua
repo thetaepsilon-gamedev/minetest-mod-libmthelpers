9c3593239d98881374c2c5b10e2ee66d545a99ba
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-10-09 21:40:41 +0100

    checkers.lua: add list equality comparison function

--- checkers.lua
+++ checkers.lua
@@ -1,4 +1,5 @@
 local check = {}
+check.selftest = {}
 
 
 
@@ -38,6 +39,49 @@ check.mkassert = function(caller)
 			error(caller..": "..message)
 		end
 	end
+end
+
+
+
+-- the following functions do NOT throw errors but are tests only
+-- used among other things for the set datatype self-tests in the assertion statements.
+local tabletest = function(val)
+	return type(val) == "table"
+end
+
+local listequal = function(a, b)
+	if not tabletest(a) or not tabletest(b) then
+		return false
+	end
+	-- doesn't really detect non-listlike tables...
+	if #a ~= #b then return false end
+	for index, value in ipairs(a) do
+		if value ~= b[index] then return false end
+	end
+
+	return true
+end
+check.listequaltest = listequal
+
+check.selftest.listequal = function()
+	local name = "modhelpers.check.selftest.listequal()"
+	local assert = check.mkassert(name)
+
+	assert(listequal({}, {}), "null lists should compare equal")
+	assert(not listequal({}, { 1 }), "null and one-item list should NOT be equal")
+	assert(not listequal({}, { 1, 2 }), "null and two-item list should NOT be equal")
+
+	assert(listequal({1}, {1}), "identical one-item lists should compare equal")
+	local sameref = { 2 }
+	assert(listequal(sameref, sameref), "duplicate reference to one-item list should compare equal")
+	assert(not listequal({42}, {4}), "differing one-item lists should NOT compare equal")
+	assert(not listequal({3, 4}, {5}), "one- and two-item lists should NOT compare equal")
+
+	assert(listequal({6, 7}, {6, 7}), "identical two-item lists should compare equal")
+	assert(not listequal({6, 7}, {6, 8}), "differing two-item lists should NOT compare equal")
+	assert(not listequal({6, 7}, {7, 6}), "reversed-order two-item lists should NOT compare equal")
+
+	return name.." self-tests completed successfully"
 end
 
 
