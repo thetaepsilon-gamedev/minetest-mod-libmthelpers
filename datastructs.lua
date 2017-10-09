706d2360acc51a8642a9737f1f6859b456ae8df1
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-10-09 22:58:16 +0100

    datastructs.lua: add size operation to set

--- datastructs.lua
+++ datastructs.lua
@@ -11,6 +11,7 @@ local iterate_values_co = function(t) for _, v in pairs(t) do coroutine.yield(v)
 datastructs.new.set = function()
 	local unique = function(val) return type(val).."!"..tostring(val) end
 	local entries = {}
+	local size = 0
 
 	-- check if an entry is present without inserting it; returns true if so.
 	local test = function(v)
@@ -24,19 +25,28 @@ datastructs.new.set = function()
 		add = function(v)
 			local key = unique(v)
 			local isnew = (entries[key] == nil)
-			if isnew then entries[key] = v end
+			if isnew then
+				entries[key] = v
+				size = size + 1
+			end
 			return isnew
 		end,
 		-- returns true if item was removed, false if it didn't exist
 		remove = function(v)
 			local didexist = test(v)
-			if didexist then entries[unique(v)] = nil end
+			if didexist then
+				entries[unique(v)] = nil
+				size = size - 1
+			end
 			return didexist
 		end,
 		ismember = test, -- see above
 		iterator = function()
 			return mkiterator(iterate_values_co, entries)
 		end,
+		size = function()
+			return size
+		end
 	}
 end
 
@@ -64,6 +74,7 @@ datastructs.selftest.set = function()
 	local t = {}
 	local collected = nil
 
+	assert(set.size() == 0, "new set should have size zero")
 	assert(#collect_set(set) == 0, "new set should return zero elements")
 
 	local notexists = "object should not be considered present if not previously inserted"
@@ -74,10 +85,12 @@ datastructs.selftest.set = function()
 	assert(set.ismember(t), "object should be member after being inserted")
 	collected = collect_set(set)
 	assert(#collected == 1, "set should return one element after insertion")
+	assert(set.size() == 1, "set should have size one after insertion")
 	assert(collected[1] == t, "sole item of set should be the inserted element")
-	assert(set.remove(t), "object should have been removed after being inserted")
-	assert(#collect_set(set) == 0, "set should return zero elements after removal")
 
+	assert(set.remove(t), "object should have been removed after being inserted")
+	assert(set.size() == 0, "set size should be zero after removal")
+	assert(#collect_set(set) == 0, "set should return zero elements after removal")
 	local oldelement = "item should not be present if added then removed"
 	assert(not set.remove(t), oldelement)
 	assert(not set.ismember(t), oldelement)
@@ -86,8 +99,11 @@ datastructs.selftest.set = function()
 	assert(set.add(1), newelement)
 	assert(set.add(2), newelement)
 	assert(set.add(3), newelement)
+	assert(set.size() == 3, "set should have size 3 after three insertions")
 	assert(#collect_set(set) == 3, "set should contain three elements after three insertions")
+
 	assert(set.remove(2), "object should have been removed")
+	assert(set.size() == 2, "set should have size 2 after three insertions and one removal")
 	assert(#collect_set(set) == 2, "set should contain two elements after three insertions and one removal")
 
 	assert(not set.remove(4), notexists)
