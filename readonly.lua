6d89055fd72a66290cdc593cdccc17792ae2846a
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-10-22 09:22:29 +0100

    readonly.lua: move shallowcopy() to tableutils.lua, set up compat alias

--- readonly.lua
+++ readonly.lua
@@ -4,17 +4,7 @@
 
 local readonly = {}
 
-readonly.shallowcopy = function(input)
-	-- table is the only type that needs this protection, can't duplicate a userdata from lua code...
-	-- all other types are immutable values.
-	if type(input) ~= "table" then error("only tables can be shallow copied") end
-
-	local ret = {}
-	for k, v in pairs(input) do
-		ret[k] = v
-	end
-
-	return ret
-end
+-- used to be here, now aliased from tableutils for compatibilty
+readonly.shallowcopy = modhelpers.tableutils.shallowcopy
 
 return readonly
--- tableutils.lua
+++ tableutils.lua
@@ -16,4 +16,16 @@ tableutils.grep = function(table, f)
 	return ret
 end
 
+tableutils.shallowcopy = function(input)
+	-- table is the only type that this works for, can't truly duplicate a userdata from lua code.
+	if type(input) ~= "table" then error("only tables can be shallow copied") end
+
+	local ret = {}
+	for k, v in pairs(input) do
+		ret[k] = v
+	end
+
+	return ret
+end
+
 return tableutils
