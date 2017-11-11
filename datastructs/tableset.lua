21216944ddc0b2cc19de8cdaa960bd7baac54a57
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-11-11 21:10:40 +0000

    datastructs: tableset: allow use of specifiable hasher to map keys, use to implement existing tableset with no-op hasher

--- datastructs/tableset.lua
+++ datastructs/tableset.lua
@@ -3,15 +3,19 @@ local interface = {}
 local iterators = _deps.tableset.iterators
 local mk_value_iterator = iterators.mk_value_iterator
 
--- simplified version of the set data structure for working with table or userdata handles;
--- it is assumed that inserted values do not alias inside the table (think 0 and "0").
-local new = function()
+-- generic table which takes a "hash function" to calculate keys.
+-- the hash function is expected to produce a "compare equal" quality:
+-- for the same value, the hash result must be the same.
+-- two values which are considered equal for this hasher
+-- (but do not compare directly, e.g. coordinate tables with the same values but different addresses)
+-- must also map to the same hash value.
+local mk_generic = function(hasher)
 	local entries = {}
 	local size = 0
 
 	-- check if an entry is present without inserting it; returns true if so.
 	local test = function(v)
-		local exists = (entries[v] ~= nil)
+		local exists = (entries[hasher(v)] ~= nil)
 		return exists
 	end
 
@@ -19,9 +23,10 @@ local new = function()
 
 	-- returns true if object was added, false if duplicate insertion
 	interface.add = function(v)
-		local isnew = (entries[v] == nil)
+		local hash = hasher(v)
+		local isnew = (entries[hash] == nil)
 		if isnew then
-			entries[v] = v
+			entries[hash] = v
 			size = size + 1
 		end
 		return isnew
@@ -29,9 +34,10 @@ local new = function()
 
 	-- returns true if item was removed, false if it didn't exist
 	interface.remove = function(v)
-		local didexist = test(v)
+		local hash = hasher(v)
+		local didexist = (entries[hash] ~= nil)
 		if didexist then
-			entries[v] = nil
+			entries[hash] = nil
 			size = size - 1
 		end
 		return didexist
@@ -49,6 +55,13 @@ local new = function()
 	
 	return interface
 end
-interface.new = new
+interface.mk_generic = mk_generic
+
+-- simplified version of the set data structure for working with table or userdata handles;
+-- it is assumed that inserted values do not alias inside the table (think 0 and "0").
+-- hasher is a no-op and passes keys directly to the table to let it do hashing on values internally.
+interface.new = function()
+	return mk_generic(function(v) return v end)
+end
 
 return interface
