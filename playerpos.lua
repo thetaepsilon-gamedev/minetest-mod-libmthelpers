fb2aa1bc8d2f2a522c0eefced381a29139ce68e6
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-11-26 16:26:42 +0000

    playerpos.lua: remove unused getstoodnode() and getstoodmeta(), use mtrequire() for retrieving tableutils

--- playerpos.lua
+++ playerpos.lua
@@ -10,7 +10,8 @@ playerpos.bias = posbias
 
 
 -- flooring alone isn't appropriate when the block at X contains coords of X-1.
-local tablefilter = modhelpers.tableutils.filter
+local tableutils = mtrequire("com.github.thetaepsilon.minetest.libmthelpers.tableutils")
+local tablefilter = tableutils.filter
 
 local pos_center_on_node = function(pos)
 	return tablefilter(posbias(pos, 0.5, 0.5, 0.5), math.floor)
@@ -25,20 +26,6 @@ local playerstoodnode = function(playerref)
 	return pos_center_on_node(posbias(playerref:get_pos(), 0.0, -0.5, 0.0))
 end
 playerpos.stoodnode = playerstoodnode
-
-
-
-playerpos.getstoodnode = function(playerref)
-	local pos = playerstoodnode(playerref)
-	return { data = minetest.get_node(pos), pos = pos }
-end
-
-
-
-playerpos.getstoodmeta = function(playerref)
-	local pos = playerstoodnode(playerref)
-	return minetest.get_meta(pos):to_table().fields
-end
 
 
 
