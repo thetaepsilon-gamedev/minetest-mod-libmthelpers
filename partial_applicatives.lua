3f893b66a56852e282b28eac8ca913cc41f25250
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-11-26 17:06:48 +0000

    partial_applicatives.lua: refactor to make currymethod() work based on string key instead of direct method object passing

--- partial_applicatives.lua
+++ partial_applicatives.lua
@@ -4,29 +4,35 @@ local curry = {}
 local check = mtrequire("com.github.thetaepsilon.minetest.libmthelpers.check")
 local mkfnexploder = check.mkfnexploder
 
+
+
 -- partially applies the "self" object of a table method function into a closure.
 -- that is, f(self, a, ...) becomes g(a, ...) and "self" is captured by closure scope of the function.
 -- the function implicitly retains a handle to the original table,
 -- and can be passed around invididually.
-local currymethod = function(tbl, m)
+local check_currymethod = mkfnexploder("curry.method()")
+local currymethod = function(tbl, mkey)
+	local check = check_currymethod
+	local m = tbl[mkey]
+	check(m, "table method "..tostring(mkey))
 	return function(...)
 		return m(tbl, ...)
 	end
 end
 curry.method = currymethod
 
-local check_curryobject = mkfnexploder("curryobject()")
+
+
 -- curry a list of methods from an existing table.
 local curryobject = function(tbl, list)
-	local check = check_curryobject
 	local result = {}
 	for _, key in ipairs(list) do
-		local m = tbl[key]
-		check(m, "table method "..tostring(key))
-		result[key] = currymethod(tbl, m)
+		result[key] = currymethod(tbl, key)
 	end
 	return result
 end
 curry.object = curryobject
+
+
 
 return curry
