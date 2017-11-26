06ee15249a87fc1cb6a853d161cb016e32648d64
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-11-26 19:23:49 +0000

    move master interface setup to mtrequire-invoked script

--- init.lua
+++ init.lua
@@ -2,31 +2,5 @@ local componentbase = "com.github.thetaepsilon.minetest.libmthelpers"
 
 
 
--- sub-components to register, and their origin files.
-local regtable = {
-	-- except where noted, all of the below are portable and can be loaded outside MT.
-	{ "prettyprint" },
-	{ "iterators" },
-	{ "tableutils" },
-	{ "coords" },
-	-- portable, though stoodnode expects the interface of an objectref.
-	{ "playerpos" },
-	{ "check", "checkers" },	-- filename doesn't fit pattern
-	{ "facedir" },
-	{ "stats" },
-	{ "readonly" },
-	{ "datastructs", nil, true },
-	{ "continuations" },
-	{ "profiling" },
-}
-
--- see below: a plain list of just component names
-local subs = {}
-for _, entry in ipairs(regtable) do
-	local componentname = entry[1]
-	table.insert(subs, componentname)
-end
-
--- test usage of the modns parent-namespace helper
-local master = modns.mk_parent_ns_noauto(subs, componentbase, ".")
-modns.register(componentbase, master, false)
+-- test load the entire interface
+local master = mtrequire(componentbase)
---
+++ lib/com.github.thetaepsilon.minetest.libmthelpers.lua
@@ -0,0 +1,19 @@
+-- except where noted, all of the below are portable and can be loaded outside MT.
+local components = {
+	"prettyprint",
+	"iterators",
+	"tableutils",
+	"coords",
+	"playerpos",	-- portable, though stoodnode expects the interface of an objectref.
+	"check",
+	"facedir",
+	"stats",
+	"readonly",
+	"datastructs",
+	"continuations",
+	"profiling",
+}
+
+
+
+return modns.mk_parent_ns(components)
